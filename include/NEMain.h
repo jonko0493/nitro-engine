06a1f37f3562e8a43f78955b66b1fdcf0e38e21f
Antonio Niño Díaz <antonio_nd@proton.me>
2026-06-16 00:02:29 +0100

    docs: library: Update changelog and library for version 0.16.0

--- include/NEMain.h
+++ include/NEMain.h
@@ -50,9 +50,9 @@ extern "C" {
 /// Major version of Nitro Engine
 #define NITRO_ENGINE_MAJOR (0)
 /// Minor version of Nitro Engine
-#define NITRO_ENGINE_MINOR (15)
+#define NITRO_ENGINE_MINOR (16)
 /// Patch version of Nitro Engine
-#define NITRO_ENGINE_PATCH (7)
+#define NITRO_ENGINE_PATCH (0)
 
 /// Full version of Nitro Engine
 #define NITRO_ENGINE_VERSION ((NITRO_ENGINE_MAJOR << 16) |  \
@@ -60,7 +60,7 @@ extern "C" {
                               (NITRO_ENGINE_PATCH))
 
 /// String with the version of Nitro Engine
-#define NITRO_ENGINE_VERSION_STRING "0.15.7"
+#define NITRO_ENGINE_VERSION_STRING "0.16.0"
 
 /// @}
 
